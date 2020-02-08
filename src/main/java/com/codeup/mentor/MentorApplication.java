5a479c202fa9619af99ccadb14c8e5c9f2eb6e3d
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-08 08:47:08 -0600

    push up from initial commit

---
+++ src/main/java/com/codeup/mentor/MentorApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorApplication.class, args);
+    }
+
+}
