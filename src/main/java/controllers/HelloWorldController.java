29d65b6a4fcfcef1582b2e42a427c63ca4a62e87
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-10 09:38:05 -0600

    setting up hello world

---
+++ src/main/java/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+    return "helloworld";
+    }
+}
