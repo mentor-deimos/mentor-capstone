41a262bfc02c56e97b08b6fbea2a50fc853d1b44
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-10 10:18:33 -0600

    local save

---
+++ src/main/java/com/codeup/mentor/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.codeup.mentor.controllers;
+
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloWorldController {
+
+//    @GetMapping("/hello")
+//    public String sayHello(){
+//    return "helloworld.html";
+//    }
+//
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHelloRB(){
+        return "Hello from the downtowners!";
+    }
+}
