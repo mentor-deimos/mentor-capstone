89eed7549120bf629cb3cd2cfd462910d496d8e6
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-10 14:33:45 -0600

    comment out w-i-p to compile

--- src/main/java/com/codeup/mentor/controllers/SearchController.java
+++ src/main/java/com/codeup/mentor/controllers/SearchController.java
@@ -1,18 +1,18 @@
-package com.codeup.mentor.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class SearchController {
-
-    @GetMapping("/search")
-    public String Testing(){
-        return "test";
-    }
-
-    @GetMapping("/search/${myInput}")
-    public String ReturnTheInput(){
-        return "this should return the input from the navbar";
-    }
-}
+//package com.codeup.mentor.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class SearchController {
+//
+//    @GetMapping("/search")
+//    public String Testing(){
+//        return "test";
+//    }
+//
+//    @GetMapping("/search/${myInput}")
+//    public String ReturnTheInput(){
+//        return "this should return the input from the navbar";
+//    }
+//}
