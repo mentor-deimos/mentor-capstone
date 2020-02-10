5fd9598305ccdd009f7bfb8d1b87535d327c0a1a
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-10 14:06:37 -0600

    middle of creating interest entitites

---
+++ src/main/java/com/codeup/mentor/model/Interests.java
@@ -0,0 +1,22 @@
+package com.codeup.mentor.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="interests")
+public class Interests {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String picture_path;
+
+    @ManyToMany
+    Set<User> userInterest;
+
+}
