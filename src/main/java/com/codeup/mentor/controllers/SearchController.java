daefd101e6a434311c18b5211fffb3cd1355d607
Kellsey Neagley <kellseyneagley@gmail.com>
2020-02-11 11:43:56 -0600

    added search controller

--- src/main/java/com/codeup/mentor/controllers/SearchController.java
+++ src/main/java/com/codeup/mentor/controllers/SearchController.java
@@ -1,18 +1,31 @@
-//package com.codeup.mentor.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class SearchController {
-//
-//    @GetMapping("/search")
-//    public String Testing(){
-//        return "test";
-//    }
-//
-//    @GetMapping("/search/${myInput}")
-//    public String ReturnTheInput(){
-//        return "this should return the input from the navbar";
-//    }
-//}
+package com.codeup.mentor.controllers;
+
+import com.codeup.mentor.model.User;
+import com.codeup.mentor.services.SearchService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SearchController {
+    SearchService searchService;
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/search")
+    public String Testing(){
+        return "helloworld";
+    }
+
+
+    @RequestMapping(value = "/search/{myInput}", method=RequestMethod.GET)
+    public String getSearchResults(@PathVariable String myInput, User user, Model model){
+        searchService.searchForUsers(myInput, user.isIs_mentor());
+        model.addAttribute("myInput", myInput);
+
+        return "search";
+    }
+}
