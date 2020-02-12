f4ff9175d7947c8d3e09707414d2c7abb4ef56f0
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-12 14:35:57 -0600

    refactored multiple pages, inserted seed message into message.html, connected existing views together

--- src/main/java/com/codeup/mentor/controllers/MessagingController.java
+++ src/main/java/com/codeup/mentor/controllers/MessagingController.java
@@ -1,4 +1,32 @@
 package com.codeup.mentor.controllers;
 
+
+import com.codeup.mentor.model.Message;
+import com.codeup.mentor.model.User;
+import com.codeup.mentor.repositories.MessageRepository;
+import com.codeup.mentor.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MessagingController {
+    private MessageRepository messageDao;
+    private UserRepository userDao;
+
+
+    public MessagingController(MessageRepository messageDao){
+        this.messageDao = messageDao;
+        this.userDao = userDao;
+
+    }
+
+    @GetMapping("/messages")
+    public String showMessages(Model model){
+       Message currentmsg = messageDao.getOne((long) 1);
+       model.addAttribute("message", currentmsg);
+
+        return "messages";
+    }
+
 }
--- src/main/java/com/codeup/mentor/model/Interest.java
+++ src/main/java/com/codeup/mentor/model/Interest.java
@@ -21,4 +21,35 @@ public class Interest {
     @ManyToMany(mappedBy = "interestList")
     List<User> users;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture_path() {
+        return picture_path;
+    }
+
+    public void setPicture_path(String picture_path) {
+        this.picture_path = picture_path;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
