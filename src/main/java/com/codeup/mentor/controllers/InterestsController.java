9eaba934df4b828444e77f6aca0032d34200a52d
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-12 10:52:04 -0600

    refactor usercontroller, refactor endpoints for connecting view) ;

--- src/main/java/com/codeup/mentor/controllers/InterestsController.java
+++ src/main/java/com/codeup/mentor/controllers/InterestsController.java
@@ -1,5 +1,10 @@
 package com.codeup.mentor.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Controller
 public class InterestsController{
 
