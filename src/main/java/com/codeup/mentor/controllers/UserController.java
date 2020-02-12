64dda8541a94d6763988aad53fd1576fe441512e
Kellsey Neagley <kellseyneagley@gmail.com>
2020-02-12 11:27:34 -0600

    fixed

--- src/main/java/com/codeup/mentor/controllers/UserController.java
+++ src/main/java/com/codeup/mentor/controllers/UserController.java
@@ -2,30 +2,43 @@ package com.codeup.mentor.controllers;
 
 
 import com.codeup.mentor.model.User;
+import com.codeup.mentor.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class UserController {
-    private User users;
+    private UserRepository userDao;
 //    private PasswordEncoder passwordEncoder;
 
-    public UserController(User users
+    public UserController(UserRepository userDao
 //    , PasswordEncoder passwordEncoder
 
     ){
-        this.users = users;
+        this.userDao = userDao;
 //        this.passwordEncoder = passwordEncoder;
 
+    }
+
+    @GetMapping("/mentor")
+    public String showSplashPage(){
+        return "splash";
+    }
+
+    //    navbar
+    @GetMapping("/navbarTest")
+    public String navbarTest(){
+        return "partials/navbar";
     }
 
     @GetMapping("/signup")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
-        return "users/signup";
+        return "signUp";
     }
 
     @PostMapping("/signup")
