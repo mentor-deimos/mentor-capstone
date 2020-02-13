c41c462f781769b7fe496bd59900c321af67570a
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-13 15:31:44 -0600

    profile controller created to direct to profile page, loads dummy profile.html, navbar refactored to include img assets appropriately and send to profile page

---
+++ src/main/java/com/codeup/mentor/controllers/ProfileController.java
@@ -0,0 +1,18 @@
+package com.codeup.mentor.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+
+    @GetMapping("/profile")
+    public String showProfile(){
+
+        return "profile";
+    }
+
+
+}
