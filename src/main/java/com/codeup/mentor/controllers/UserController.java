c25861f1379d53cd4c4a12c991e43254b33c1afa
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-18 14:33:54 -0600

    filestack url capture tested successfully, built a user through to home

--- src/main/java/com/codeup/mentor/controllers/UserController.java
+++ src/main/java/com/codeup/mentor/controllers/UserController.java
@@ -11,16 +11,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 import javax.persistence.GeneratedValue;
 import java.util.List;
 
 
 @Controller
 public class UserController {
+
+    private String uploadHandle;
+
     @Autowired
     private UserRepository userDao;
     @Autowired
@@ -48,14 +49,10 @@ public class UserController {
         return "signUp";
 }
 
-//    @GetMapping("/home")
-//    public String showHomePage(){
-//        return "home";
-//    }
-
     @PostMapping("/signup")
     public String saveUser(@ModelAttribute User user){
         String hash = passwordEncoder.encode(user.getPassword());
+//        user.setFilestack_picture_url(filestackURL);
         user.setPassword(hash);
         userDao.save(user);
         return "home";
