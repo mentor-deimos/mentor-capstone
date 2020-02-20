0f44737b91ca17d3946d48a23f3e5486efea2639
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-20 10:27:31 -0600

    plugging thymeleaf into profile to pull user info

--- src/main/java/com/codeup/mentor/controllers/ProfileController.java
+++ src/main/java/com/codeup/mentor/controllers/ProfileController.java
@@ -1,18 +1,33 @@
 package com.codeup.mentor.controllers;
 
 
+import com.codeup.mentor.model.User;
+import com.codeup.mentor.repositories.InterestRepository;
+import com.codeup.mentor.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class ProfileController {
+    private UserRepository userDao;
+    private InterestRepository interestDao;
 
 
-//    @GetMapping("/profile")
-//    public String showProfile(){
-//
-//        return "profile";
-//    }
+    @GetMapping("/profile")
+    public String goToProfile(Model model, Principal principal){
+        if (principal != null){
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("user", user);
+//            model.addAttribute("rating", ratingService.allRatingsOnSearch(2));
+            return "profile";
+        }
+        return "/splash";
 
+
+    }
 
 }
