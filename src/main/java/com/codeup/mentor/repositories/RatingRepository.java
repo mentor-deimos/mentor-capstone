4f18967d3acec3f6f9784dd0df49757e8dd5cbe9
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-02-25 15:42:47 -0600

    rating > controller CHECKS for matching reviews of currentuser vs profileuser > if review array empty, review away otherwise div killed to avoid multi reviews

--- src/main/java/com/codeup/mentor/repositories/RatingRepository.java
+++ src/main/java/com/codeup/mentor/repositories/RatingRepository.java
@@ -10,4 +10,8 @@ import java.util.List;
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     @Query(value ="SELECT * FROM ratings WHERE recipient_user_id = ?1", nativeQuery = true)
     List<Rating> findAllByRecipientId(long id);
+
+    @Query(value="SELECT * FROM ratings WHERE recipient_user_id = ?1 AND giver_user_id = ?2", nativeQuery = true)
+    List<Rating> findByGiver_infoExists(long recipient_id, long giver_id);
+
 }
